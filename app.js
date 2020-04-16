202149566163ee68f10d7682c80e14f7bb986483
Angelika Villanueva <angelika@Angelikas-MacBook-Pro.local>
2020-04-16 20:02:17 +0800

    Level 4: Salting and Hashing

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
@@ -20,26 +26,30 @@ const userSchema = new mongoose.Schema ({
 const User = new mongoose.model("User", userSchema);
 
 app.route("/")
+
   .get(function(req, res) {
-      res.render("home");
+    res.render("home");
   });
 
 app.route("/login")
   .get(function(req, res) {
     res.render("login");
   })
+
   .post(function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(e, foundUser) {
       if (e) {
         console.log(e);
       } else {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          console.log("account not found")
+        if (foundUser) {
+          bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+            if(result == true) {
+              res.render("secrets");
+            }
+          });
         }
       }
     });
@@ -47,20 +57,24 @@ app.route("/login")
 
 app.route("/register")
   .get(function(req, res) {
-      res.render("register");
+    res.render("register");
   })
+
   .post(function(req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+      newUser.save(function(e) {
+        if (e) {
+          console.log(e);
+        } else {
+          res.render("secrets");
+        }
+      });
     });
-    newUser.save(function(e) {
-      if (e) {
-        console.log(e);
-      } else {
-        res.render("secrets");
-      }
-    });
+
   });
 
 
