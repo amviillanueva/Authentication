62205876a460ff2b8c75734031198903661d141e
Angelika Villanueva <angelika@Angelikas-MacBook-Pro.local>
2020-04-19 21:28:00 +0800

    Failed adding facebook oauth

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
+const FacebookStrategy = require('passport-facebook');
 
 const app = express();
 
@@ -66,6 +67,20 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    enableProof: true
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log("hi");
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.route("/")
   .get(function(req, res) {
     res.render("home");
@@ -78,6 +93,15 @@ app.get("/auth/google/secrets",
   passport.authenticate("google", { failureRedirect: "/login" }),
     function(req, res) {
 
+    res.redirect("/secrets");
+  });
+
+app.get("/auth/facebook",
+  passport.authenticate("facebook", {scope: ["user-friends"]}));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function(req, res) {
     res.redirect("/secrets");
   });
 
